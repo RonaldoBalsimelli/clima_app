d1e5a9f338210c08b2ac26661cf61a916dda8cc3
Ronaldo Balsimelli <ronaldobalsimelli@hotmail.com>
2025-04-28 17:58:50 -0300

    adicionado icone de dark mode, adicionado toas informacoes da api

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -6,8 +6,21 @@ class WeatherController < ApplicationController
     weather_data = get_weather(city)
 
     if weather_data
+      @city_name = weather_data['name']
       @temperature = weather_data['main']['temp']
+      @feels_like = weather_data['main']['feels_like']
+      @temp_min = weather_data['main']['temp_min']
+      @temp_max = weather_data['main']['temp_max']
+      @humidity = weather_data['main']['humidity']
+      @pressure = weather_data['main']['pressure']
       @description = weather_data['weather'].first['description']
+      @wind_speed = weather_data['wind']['speed']
+      @wind_deg = weather_data['wind']['deg']
+      @clouds = weather_data['clouds']['all']
+      @visibility = weather_data['visibility']
+      @country = weather_data['sys']['country']
+      @sunrise = Time.at(weather_data['sys']['sunrise']).strftime("%H:%M")
+      @sunset = Time.at(weather_data['sys']['sunset']).strftime("%H:%M")
     else
       flash[:alert] = "Cidade não encontrada!"
     end
@@ -18,22 +31,14 @@ class WeatherController < ApplicationController
   private
 
   def get_weather(city)
-    api_key = '099ab0a9bad88a8b1c6631e49cfabbb2'  # Substitua pela sua chave da API
-
-    # Codificar a cidade para tratar caracteres especiais
+    api_key = '099ab0a9bad88a8b1c6631e49cfabbb2'
     city_encoded = CGI.escape(city)
-
-    # Construir a URL completa da API, agora usando o domínio correto
     url = "https://api.openweathermap.org/data/2.5/weather?q=#{city_encoded}&appid=#{api_key}&units=metric&lang=pt_br"
-
-    # Fazendo a requisição GET para a API
     response = self.class.get(url)
 
-    # Debug: Verificando a resposta da API
     if response.success?
       return response.parsed_response
     else
-      Rails.logger.error "Erro ao buscar dados da cidade: #{response.code} - #{response.message}"
       return nil
     end
   end
